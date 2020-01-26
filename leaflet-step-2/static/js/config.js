24718b3d1e6c2be31dcb71ba1f8949da3a44e7c0
justinfoust <54191450+justinfoust@users.noreply.github.com>
2020-01-26 14:07:51 -0800

    part 2 complete

---
+++ leaflet-step-2/static/js/config.js
@@ -0,0 +1,2 @@
+// API key
+const API_KEY = "pk.eyJ1IjoiamZvdXN0IiwiYSI6ImNrNW9veTVtazA0eHQza3FoZmI3b3gxNDgifQ.ecvXzuUG2FS0X3E5SoyAkw";
--- leaflet-step-2/static/js/logic.js
+++ leaflet-step-2/static/js/logic.js
@@ -18,20 +18,11 @@ var usgsURL = {
     month: "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson"
 };
 
+var boundariesFilePath = "static/data/boundaries.json"
+
 
 //
 //===================================================================================
-
-var myMap = L.map("map", {
-    center: [40, -110],
-    zoom: 5
-});
-
-var outdoorMap = L.tileLayer(mapbox.apiURL, {
-    attribution: mapbox.attribution,
-    id: mapbox.outdoors,
-    accessToken: API_KEY
-}).addTo(myMap);
 
 
 function magColor(mag) {
@@ -41,10 +32,61 @@ function magColor(mag) {
         case 2: return "yellow"; break;
         case 3: return "orange"; break;
         case 4: return "darkOrange"; break;
-        case 5: return "red"; break;
+        default: return "red"
     }
 }
 
+var layers = {
+    earthquakeLayer: new L.LayerGroup(),
+    tectonicLayer: new L.LayerGroup()
+};
+
+
+var baseMaps = {
+
+    "Street Map": L.tileLayer(mapbox.apiURL, {
+        attribution: mapbox.attribution,
+        id: mapbox.street,
+        accessToken: API_KEY
+    }),
+    "Outdoor Map": L.tileLayer(mapbox.apiURL, {
+        attribution: mapbox.attribution,
+        id: mapbox.outdoors,
+        accessToken: API_KEY
+    }),
+    "Light Map": L.tileLayer(mapbox.apiURL, {
+        attribution: mapbox.attribution,
+        id: mapbox.light,
+        accessToken: API_KEY
+    }),
+    "Dark Map": L.tileLayer(mapbox.apiURL, {
+        attribution: mapbox.attribution,
+        id: mapbox.dark,
+        accessToken: API_KEY
+    }),
+    "Satellite Map": L.tileLayer(mapbox.apiURL, {
+        attribution: mapbox.attribution,
+        id: mapbox.satellite,
+        accessToken: API_KEY
+    })
+};
+
+var overlayMaps = {
+    Earthquakes: layers.earthquakeLayer,
+    "Fault Lines": layers.tectonicLayer
+};
+
+
+
+var myMap = L.map("map", {
+    center: [40, -110],
+    zoom: 5,
+    layers: [baseMaps["Dark Map"], layers.earthquakeLayer, layers.tectonicLayer]
+});
+
+L.control.layers(baseMaps, overlayMaps, {
+    collapsed: false
+}).addTo(myMap);
 
 var info = L.control({
     position: "bottomright"
@@ -65,13 +107,10 @@ info.addTo(myMap);
 
 
 d3.json(usgsURL.week).then(data => {
-
     data.features.map(d => {
-
         L.geoJSON(d, {
 
             pointToLayer: (geoJsonPoint, latlng) => {
-
                 return L.circle(latlng, {
                     fillOpacity: 0.75,
                     weight: 0,
@@ -81,13 +120,25 @@ d3.json(usgsURL.week).then(data => {
             },
 
             onEachFeature: (feature, layer) => {
-
                 layer.bindPopup("<h4>" + feature.properties.place + "</h3>" +
                                 "<hr>" +
-                                "<p>" + new Date(feature.properties.time) + "</p>");
-
+                                "<p>Magnitude: " + feature.properties.mag + "<br>" + new Date(feature.properties.time) + "</p>");
             }
-        }).addTo(myMap)
+            
+        }).addTo(layers.earthquakeLayer)
+    })
+})
 
+d3.json(boundariesFilePath).then(data => { 
+    data.features.map(d => {
+        L.geoJSON(d, {
+            
+            style: f => ({
+                color: "orange",
+                weight: 2,
+                opacity: 0.75
+            })
+            
+        }).addTo(layers.tectonicLayer)
     })
 })
